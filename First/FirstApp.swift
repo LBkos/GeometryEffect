ccf7ff4d52a9d4e7c85978ca05caefd826ca94e8
LBkos <constantin.l.o.p@gmail.com>
2023-07-11 13:38:59 +0700

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Константин Лопаткин on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
